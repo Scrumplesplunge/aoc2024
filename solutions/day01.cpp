5debbcfde03bcf72fa1bdb70e6a064af7b9c6bd4
Joe Fowler <scrumplesplunge@gmail.com>
2024-12-07 16:29:06 +0000

    Read the input into a buffer first.
    This speeds up parsing.

--- solutions/day01.cpp
+++ solutions/day01.cpp
@@ -6,45 +6,51 @@
 
 namespace aoc2024 {
 
-Task<void> Day01(tcp::Socket& socket) {
-  int a[1000];
-  int b[1000];
+struct Input {
+  Task<void> Read(tcp::Socket& socket) {
+    char buffer[14001];
+    std::span<const char> input = co_await socket.Read(buffer);
+    assert(input.size() == 14000);
 
-  std::println("parsing input...");
-  for (int i = 0; i < 1000; i++) {
-    // Here we are requiring exactly 14 bytes per line:
-    //   * 5 bytes for the first number
-    //   * 3 spaces
-    //   * 5 bytes for the second number
-    //   * A newline
-    char buffer[14];
-    std::span<const char> line = co_await socket.Read(buffer);
-    if (line.size() != 14) {
-      co_await socket.Write("bad input");
-      co_return;
+    for (int i = 0; i < 1000; i++) {
+      // Here we are requiring exactly 14 bytes per line:
+      //   * 5 bytes for the first number
+      //   * 3 spaces
+      //   * 5 bytes for the second number
+      //   * A newline
+      std::span<const char> line = input.subspan(14 * i, 14);
+      assert(line[5] == ' ' && line[6] == ' ' && line[7] == ' ');
+      assert(line[13] == '\n');
+
+      a[i] = std::stoi(std::string(line.data(), 5));
+      b[i] = std::stoi(std::string(line.data() + 8, 5));
     }
-    assert(line[5] == ' ' && line[6] == ' ' && line[7] == ' ');
-    assert(line[13] == '\n');
-
-    a[i] = std::stoi(std::string(line.data(), 5));
-    b[i] = std::stoi(std::string(line.data() + 8, 5));
   }
 
+  int a[1000];
+  int b[1000];
+};
+
+Task<void> Day01(tcp::Socket& socket) {
+  std::println("parsing input...");
+  Input input;
+  co_await input.Read(socket);
+
   std::println("sorting...");
-  std::ranges::sort(a);
-  std::ranges::sort(b);
+  std::ranges::sort(input.a);
+  std::ranges::sort(input.b);
 
   int delta = 0;
   for (int i = 0; i < 1000; i++) {
-    delta += std::abs(a[i] - b[i]);
+    delta += std::abs(input.a[i] - input.b[i]);
   }
 
   std::println("part 1: {}", delta);
 
   int score = 0;
   for (int i = 0; i < 1000; i++) {
-    auto [first, last] = std::ranges::equal_range(b, a[i]);
-    score += a[i] * (last - first);
+    auto [first, last] = std::ranges::equal_range(input.b, input.a[i]);
+    score += input.a[i] * (last - first);
   }
 
   std::println("part 2: {}", score);
