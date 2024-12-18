dcc2348951032c6cb80527cc71289596c9c12772
Joe Fowler <scrumplesplunge@gmail.com>
2024-12-18 22:45:50 +0000

    Day 17.

--- solutions/day17.cpp
+++ solutions/day17.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <generator>
 #include <print>
 
 #include "../common/api.hpp"
@@ -10,14 +11,14 @@ namespace aoc2024 {
 namespace {
 
 enum Operation : std::uint8_t {
-  kAdv,  // a /= 1 << arg
+  kAdv,  // a = a >> arg
   kBxl,  // b ^= lit
-  kBst,  // b %= 8
+  kBst,  // b = a % 8
   kJnz,  // if (A != 0) goto lit
   kBxc,  // b ^= c
   kOut,  // emit(arg)
-  kBdv,  // b = a / (1 << arg)
-  kCdv,  // c = a / (1 << arg)
+  kBdv,  // b = a >> arg
+  kCdv,  // c = a >> arg
 };
 
 struct Input {
@@ -51,15 +52,16 @@ struct Input {
     code = std::span(buffer).subspan(0, num_operations);
   }
 
-  int a, b, c;
+  std::uint64_t a, b, c;
   std::uint8_t buffer[16];
   std::span<std::uint8_t> code;
 };
 
-std::string_view Part1(Input& input, std::span<char> output_buffer) {
-  int a = input.a, b = input.b, c = input.c;
+std::generator<std::uint8_t> Run(std::uint64_t a, std::uint64_t b,
+                                 std::uint64_t c,
+                                 std::span<const std::uint8_t> code) {
   int pc = 0;
-  const auto combo = [&](std::uint8_t x) -> int {
+  const auto combo = [&](std::uint8_t x) -> std::uint64_t {
     if (x < 4) return x;
     switch (x) {
       case 4: return a;
@@ -68,20 +70,12 @@ std::string_view Part1(Input& input, std::span<char> output_buffer) {
     }
     std::abort();
   };
-  char* out = output_buffer.data();
-  int space = output_buffer.size();
-  const auto emit = [&](int x) {
-    auto [i, n] = std::format_to_n(out, space, "{},", x);
-    if (n > space) throw std::runtime_error("output too long");
-    space -= n;
-    out = i;
-  };
-  while (pc + 1 < int(input.code.size())) {
-    const std::uint8_t op = input.code[pc], arg = input.code[pc + 1];
+  while (pc + 1 < int(code.size())) {
+    const std::uint8_t op = code[pc], arg = code[pc + 1];
     pc += 2;
     switch (op) {
       case kAdv:
-        a /= 1 << combo(arg);
+        a = a >> combo(arg);
         break;
       case kBxl:
         b ^= arg;
@@ -96,19 +90,56 @@ std::string_view Part1(Input& input, std::span<char> output_buffer) {
         b ^= c;
         break;
       case kOut:
-        emit(combo(arg) % 8);
+        co_yield combo(arg) % 8;
         break;
       case kBdv:
-        b = a / (1 << combo(arg));
+        b = a >> combo(arg);
         break;
       case kCdv:
-        c = a / (1 << combo(arg));
+        c = a >> combo(arg);
         break;
     }
+  }
+}
+
+std::string_view Part1(Input& input, std::span<char> output_buffer) {
+  char* out = output_buffer.data();
+  int space = output_buffer.size();
+  for (std::uint8_t value : Run(input.a, input.b, input.c, input.code)) {
+    auto [i, n] = std::format_to_n(out, space, "{},", value);
+    if (n > space) throw std::runtime_error("output too long");
+    space -= n;
+    out = i;
   }
   if (out == output_buffer.data()) throw std::runtime_error("no output");
   assert(out[-1] == ',');
   return std::string_view(output_buffer.data(), out - 1);
+}
+
+// Given a `prefix` value for `a` which will result in the suffix
+// `code.subspan(n + 1)` being output, search for a value of `a` which will
+// result in the entire of `code` being output.
+//
+// This relies on the assumption that:
+//
+//   * The only state that survives across loop iterations is the value of `a`.
+//   * Each loop iteration produces exactly one output and shifts `a` by 3, in
+//     some order.
+std::uint64_t FindMatch(std::uint64_t prefix, Input& input, int n) {
+  const std::span<const std::uint8_t> expected = input.code.subspan(n);
+  for (std::uint8_t j = 0; j < 8; j++) {
+    const std::uint64_t a = prefix << 3 | j;
+    if (std::ranges::equal(Run(a, input.b, input.c, input.code), expected)) {
+      if (n == 0) return a;
+      const std::uint64_t result = FindMatch(a, input, n - 1);
+      if (result) return result;
+    }
+  }
+  return 0;
+}
+
+std::uint64_t Part2(Input& input) {
+  return FindMatch(0, input, input.code.size() - 1);
 }
 
 }  // namespace
@@ -117,12 +148,13 @@ Task<void> Day17(tcp::Socket& socket) {
   Input input;
   co_await input.Read(socket);
 
-  char part1_buffer[256];
-  std::string_view part1 = Part1(input, part1_buffer);
-  std::println("part1: {}", part1);
+  char part1_buffer[128];
+  const std::string_view part1 = Part1(input, part1_buffer);
+  const std::uint64_t part2 = Part2(input);
+  std::println("part1: {}\npart2: {}", part1, part2);
 
-  char result[32];
-  const char* end = std::format_to(result, "{}\n", part1);
+  char result[64];
+  const char* end = std::format_to(result, "{}\n{}\n", part1, part2);
   co_await socket.Write(std::span<const char>(result, end - result));
 }
 
