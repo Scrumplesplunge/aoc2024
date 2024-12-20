6d52f6fcb084acb620642654f3b86814ff51624b
Joe Fowler <scrumplesplunge@gmail.com>
2024-12-20 23:28:39 +0000

    Much faster solution for Day 2.
    The speed-up comes from using a Trie for the list of towels.

--- solutions/day19.cpp
+++ solutions/day19.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <generator>
 #include <print>
 
 #include "../common/api.hpp"
@@ -13,54 +14,92 @@ struct Word { std::string_view value; };
 
 bool ScanValue(std::string_view& input, Word& word) {
   const auto i = input.find_first_not_of("bgruw");
-  if (i == input.npos) return false;
+  if (i == 0 || i == input.npos) return false;
   word.value = input.substr(0, i);
   input.remove_prefix(i);
   return true;
 }
+
+int ToIndex(char c) {
+  switch (c) {
+    case 'b': return 0;
+    case 'g': return 1;
+    case 'r': return 2;
+    case 'u': return 3;
+    case 'w': return 4;
+  }
+  throw std::runtime_error("bad stripe");
+}
+
+class TowelTrie {
+ public:
+  void Add(std::string_view towel) {
+    std::uint16_t i = 0;
+    for (char c : towel) {
+      std::uint16_t& branch = nodes_[i].next[ToIndex(c)];
+      if (branch == 0) {
+        if (num_nodes_ == kMaxNodes) throw std::runtime_error("too many nodes");
+        branch = num_nodes_++;
+      }
+      i = branch;
+    }
+    nodes_[i].is_end = true;
+  }
+
+  std::generator<int> PrefixMatches(std::string_view string) const {
+    std::uint16_t i = 0;
+    for (int c = 0, n = string.size(); c < n; c++) {
+      std::uint16_t branch = nodes_[i].next[ToIndex(string[c])];
+      if (branch == 0) co_return;
+      i = branch;
+      if (nodes_[i].is_end) co_yield c + 1;
+    }
+  }
+
+ private:
+  static constexpr int kMaxNodes = 2048;
+  struct Node {
+    bool is_end;
+    std::uint16_t next[5];
+  };
+  Node nodes_[kMaxNodes] = {};
+  int num_nodes_ = 1;
+};
 
 struct Input {
   Task<void> Read(tcp::Socket& socket) {
     std::string_view input(co_await socket.Read(buffer));
     Word towel;
     if (!ScanPrefix(input, "{}", towel)) throw std::runtime_error("syntax");
-    int num_strings = 0;
-    strings[num_strings++] = towel.value;
-    while (ScanPrefix(input, ", {}", towel)) {
-      if (num_strings == kMaxStrings) throw std::runtime_error("too many");
-      strings[num_strings++] = towel.value;
-    }
+    towels.Add(towel.value);
+    while (ScanPrefix(input, ", {}", towel)) towels.Add(towel.value);
     if (!ScanPrefix(input, "\n\n")) throw std::runtime_error("syntax");
-    const int num_towels = num_strings;
-    towels = std::span(strings).subspan(0, num_towels);
-    Word pattern;
-    while (ScanPrefix(input, "{}\n", pattern)) {
-      if (num_strings == kMaxStrings) throw std::runtime_error("too many");
-      strings[num_strings++] = pattern.value;
+    Word design;
+    int num_designs = 0;
+    while (ScanPrefix(input, "{}\n", design)) {
+      if (num_designs == kMaxDesigns) throw std::runtime_error("too many");
+      design_buffer[num_designs++] = design.value;
     }
-    const int num_patterns = num_strings - num_towels;
-    patterns = std::span(strings).subspan(num_towels, num_patterns);
-    std::println("{} strings", num_strings);
+    designs = std::span(design_buffer).subspan(0, num_designs);
   }
 
   char buffer[25000];
-  static constexpr int kMaxStrings = 2048;
-  std::string_view strings[kMaxStrings];
-  std::span<const std::string_view> towels;
-  std::span<const std::string_view> patterns;
+
+  TowelTrie towels;
+
+  static constexpr int kMaxDesigns = 512;
+  std::string_view design_buffer[kMaxDesigns];
+  std::span<const std::string_view> designs;
 };
 
-std::uint64_t Arrangements(std::string_view pattern,
-                 std::span<const std::string_view> towels) {
+std::uint64_t Arrangements(std::string_view pattern, const TowelTrie& towels) {
   std::uint64_t arrangements[100] = {};
   assert(pattern.size() < 100);
   arrangements[pattern.size()] = 1;
   for (int i = pattern.size(); i >= 0; i--) {
     const std::string_view suffix = pattern.substr(i);
-    for (std::string_view towel : towels) {
-      if (suffix.starts_with(towel)) {
-        arrangements[i] += arrangements[i + towel.size()];
-      }
+    for (int prefix : towels.PrefixMatches(suffix)) {
+      arrangements[i] += arrangements[i + prefix];
     }
   }
   return arrangements[0];
@@ -74,8 +113,8 @@ Task<void> Day19(tcp::Socket& socket) {
 
   int part1 = 0;
   std::uint64_t part2 = 0;
-  for (std::string_view pattern : input.patterns) {
-    const std::uint64_t arrangements = Arrangements(pattern, input.towels);
+  for (std::string_view design : input.designs) {
+    const std::uint64_t arrangements = Arrangements(design, input.towels);
     if (arrangements > 0) part1++;
     part2 += arrangements;
   }
